8568595855fcc8449418c35903b1e662f4a6f797
Patrick Muff <muff.patrick@muffag.ch>
2016-12-29 08:45:33 +0100

    Add default options

--- lib/src/password_generator.ts
+++ lib/src/password_generator.ts
@@ -1,13 +1,36 @@
 interface PasswordGeneratorOptions {
+  length: number;
   lowercaseLetters: boolean;
   uppercaseLetters: boolean;
   numbers: boolean;
   specialCharacters: boolean;
+  parts: {
+    amount: number;
+    length: number;
+    delimiter: string;
+  }
 }
 
-export class PasswordGenerator {  
-  constructor(private options: PasswordGeneratorOptions) {
-    
+const defaultOptions: PasswordGeneratorOptions = {
+  length: 30,
+  lowercaseLetters: true,
+  uppercaseLetters: true,
+  numbers: true,
+  specialCharacters: true,
+  parts: {
+    amount: 1,
+    length: 10,
+    delimiter: '-'
+  }
+}
+
+export class PasswordGenerator {
+  private static lowercaseLettersList: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  constructor(public options: PasswordGeneratorOptions = defaultOptions) {
   }
 
+  generate() {
+
+  }
 }
