38387ffeb659b998b1a73f95ebaa6c4cd0ff941c
Patrick Muff <muff.patrick@muffag.ch>
2016-12-29 14:00:48 +0100

    Use verbose output

--- lib/src/password_generator.ts
+++ lib/src/password_generator.ts
@@ -3,6 +3,7 @@ interface PasswordGeneratorOptions {
   uppercaseLetters: boolean;
   numbers: boolean;
   specialCharacters: boolean;
+  latin1Characters: boolean;
   parts: {
     amount: number;
     length: number;
@@ -15,6 +16,7 @@ const defaultOptions: PasswordGeneratorOptions = {
   uppercaseLetters: true,
   numbers: true,
   specialCharacters: true,
+  latin1Characters: false,
   parts: {
     amount: 1,
     length: 30,
@@ -27,25 +29,12 @@ export class PasswordGenerator {
   private static uppercaseLettersList: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   private static numbersList: string[] = '0123456789'.split('');
   private static specialCharactersList: string[] = '!"#%&()*+,-./:;<=>?@[\]^_`{|}~'.split('');
+  private static latin1List: string[] = '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ'.split('');
 
   constructor(public options: PasswordGeneratorOptions = defaultOptions) {
   }
 
-  setHumanReadableOptions(): void {
-    this.options = {
-      lowercaseLetters: true,
-      uppercaseLetters: false,
-      numbers: true,
-      specialCharacters: false,
-      parts: {
-        amount: 3,
-        length: 5,
-        delimiter: '-'
-      }
-    }
-  }
-
-  generate(): string {
+  generate(verbose: boolean = false): string {
     let list: string[] = [];
     let password: string = '';
 
@@ -60,6 +49,9 @@ export class PasswordGenerator {
     }
     if (this.options.specialCharacters) {
       list = list.concat(PasswordGenerator.specialCharactersList);
+    }
+    if (this.options.latin1Characters) {
+      list = list.concat(PasswordGenerator.latin1List);
     }
 
     for (let partIndex = 0; partIndex < this.options.parts.amount; partIndex++) {
@@ -77,14 +69,28 @@ export class PasswordGenerator {
       password += part;
     }
 
+    // Display password strength
+    if (verbose) {
+      const round = (input: number) => Math.round(input * 100) / 100;
+      const ageOfUniverse = 4.3 * 10 ** 17;
+      const secondsInYear = 31540000;
+      let combinations = list.length ** password.length;
+      let secs = round(combinations / (2 * 10 ** 12));
+
+      console.log(`Your password uses a set of ${list.length} characters and has a length of ${password.length}.`);
+      console.log(`There are ${combinations} possible combinations.`);
+      console.log(`It would take a supercomputer (10^12 passwords/s) ${secs} seconds to crack it.`)
+      console.log(`This is equal to ${round(secs / secondsInYear)} years or ${round(secs / ageOfUniverse)} times the current age of the universe.\n`);
+    }
+
     return password;
   }
 
-  generateMultiple(amount: number): string[] {
+  generateMultiple(amount: number, verbose: boolean = true): string[] {
     let passwords: string[] = [];
 
     for (let i = 0; i < amount; i++) {
-      passwords.push(this.generate());
+      passwords.push(this.generate(verbose));
     }
 
     return passwords;
