fe8d855a9e8e3fb927d216f75a7de14b3afd4aa0
Patrick Muff <muff.patrick@muffag.ch>
2019-04-12 07:58:45 +0200

    Switch from var to const in Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,39 +1,48 @@
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var typescript = require('gulp-typescript');
-var mocha = require('gulp-mocha');
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const typescript = require('gulp-typescript');
+const mocha = require('gulp-mocha');
 
-var config = {
-  src: [
-    'lib/src/**/*.ts',
-  ],
-  dest: 'lib/dest',
+const config = {
+  src: ['lib/src/**/*.ts'],
+  dest: 'lib/dest'
 };
 
-gulp.task('clean', gulp.series(function() {
-  return gulp.src(config.dest)
-    .pipe(clean());
-}));
+gulp.task(
+  'clean',
+  gulp.series(function() {
+    return gulp.src(config.dest).pipe(clean());
+  })
+);
 
-gulp.task('typescript', gulp.series('clean', function() {
-  return gulp.src(config.src)
-    .pipe(typescript({
-      module: 'commonjs',
-      target: 'ES6',
-      typescript: require('typescript')
-    }))
-    .pipe(gulp.dest(config.dest));
-}));
+gulp.task(
+  'typescript',
+  gulp.series('clean', function() {
+    return gulp
+      .src(config.src)
+      .pipe(
+        typescript({
+          module: 'commonjs',
+          target: 'ES6',
+          typescript: require('typescript')
+        })
+      )
+      .pipe(gulp.dest(config.dest));
+  })
+);
 
 gulp.task('build', gulp.series('typescript'));
 
-gulp.task('test', gulp.series('typescript', function() {
-  gulp.src(config.dest + '/**/*.spec.js')
-    .pipe(mocha({ reporter: 'spec' }))
-}));
+gulp.task(
+  'test',
+  gulp.series('typescript', function() {
+    gulp.src(config.dest + '/**/*.spec.js').pipe(mocha({ reporter: 'spec' }));
+  })
+);
 
-gulp.task('default', gulp.series('clean', 'typescript', function() {
-  gulp.watch([
-    config.src
-  ], ['typescript']);
-}));
+gulp.task(
+  'default',
+  gulp.series('clean', 'typescript', function() {
+    gulp.watch([config.src], ['typescript']);
+  })
+);
