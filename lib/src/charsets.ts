adc1c657c43922e1d106e20b09cdc440276f8b66
Patrick Muff <muff.pa@gmail.com>
2020-05-13 18:19:53 +0200

    perf: increase startup by ~10ms
    JSON.parse is faster than declaring a string and then splitting it.

--- lib/src/charsets.ts
+++ lib/src/charsets.ts
@@ -1,16 +1,18 @@
 /**
  * This file contains all of the character sets that password_generator.ts uses.
  */
-export const lowercaseLettersList: string[] = 'abcdefghijklmnopqrstuvwxyz'.split(
-  '',
+export const lowercaseLettersList: string[] = JSON.parse(
+  '["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]',
 );
-export const uppercaseLettersList: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
-  '',
+export const uppercaseLettersList: string[] = JSON.parse(
+  '["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]',
 );
-export const numbersList: string[] = '0123456789'.split('');
-export const specialCharactersList: string[] = '!"#%&()*+,-./:;<=>?@[]^_`{|}~'.split(
-  '',
+export const numbersList: string[] = JSON.parse(
+  '["0","1","2","3","4","5","6","7","8","9"]',
 );
-export const latin1List: string[] = '¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ'.split(
-  '',
+export const specialCharactersList: string[] = JSON.parse(
+  '["!","\\"","#","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]',
+);
+export const latin1List: string[] = JSON.parse(
+  '["¡","¢","£","¤","¥","¦","§","¨","©","ª","«","¬","®","¯","°","±","²","³","´","µ","¶","·","¸","¹","º","»","¼","½","¾","¿","À","Á","Â","Ã","Ä","Å","Æ","Ç","È","É","Ê","Ë","Ì","Í","Î","Ï","Ð","Ñ","Ò","Ó","Ô","Õ","Ö","×","Ø","Ù","Ú","Û","Ü","Ý","Þ","ß","à","á","â","ã","ä","å","æ","ç","è","é","ê","ë","ì","í","î","ï","ð","ñ","ò","ó","ô","õ","ö","÷","ø","ù","ú","û","ü","ý","þ","ÿ"]',
 );
